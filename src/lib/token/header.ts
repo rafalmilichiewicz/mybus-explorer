5aa0828d58dba7c76cb074f25a4d0e24a7610d81
rafal <rafal.milichiewicz@gmail.com>
2025-09-24 13:39:33 +0200

    revert 15a24071e09ee4ce3c296f8ce18a6fb10acf9b04
    revert dev init

--- src/lib/token/header.ts
+++
@@ -1,9 +0,0 @@
-import { LUBLIN_OFFSET, USER_AGENT } from '../consts/magic-numbers.ts';
-import generateToken from './token.ts';
-
-export default async function generateHeaders(ageHeader: number) {
-    return {
-        'Age': `${await generateToken(ageHeader) + LUBLIN_OFFSET}`,
-        'User-Agent': USER_AGENT,
-    };
-}
