cd5d141359a55b847f42d808d1b38d02c9aa39cc
rafal_milichiewicz <53575748+rafalmilichiewicz@users.noreply.github.com>
2025-10-08 16:44:37 +0200

    Vehicle 1

---
+++ src/lib/vehicles/vehicle.ts
@@ -0,0 +1,81 @@
+import { RouteDirection, TransportMode, VehicleType } from '../db/schema/ztm-types.ts';
+
+const _example = {
+    // Id of some sort
+    // - sometimes vehicle's sideNumber
+    // - sometimes long autoincementing
+    // - definitely not unix timestamp
+    'id': '24014', // ^
+    'nb': '24014', // "Long" side number - vehicle id
+    'nr': '25', // Route number
+    'wt': 'B', // Route variant
+    'kr': 'T', // Route direction (inbound/outbound/depot) T | P | Z
+    'ik': '2085', // ? Current destination Id
+    'lp': '18', // Number of next stop on route (1 indexed)
+    'dp': '768', // ?
+    'dw': '935', // ?
+    'x': '22.54847', // Longitude
+    'y': '51.21101', // Latitude
+    'px': '22.54848', // ?
+    'py': '51.21103', // ?
+    'o': '-90', // Delay - if positive advance else delay
+    's': '1', // Vehicle status
+    'p': '14:17', // Time of planned departure (from first stop)
+    'op': 'PANCERNIAKÓW  FELICITY', // Current destination
+    'c': 'BKN', // Icons - vehicle attributes - AC / low-floor and such
+    'nk': '2086', // ? Next destination Id
+    'nnr': '25', // Next route number
+    'nwt': 'C', // Next route variant
+    'nkr': 'P', // Next route direction
+    'nop': 'PRAWIEDNIKI', // Next destination
+    'is': '0',
+    'vt': 'A', // Vehicle type - same as in db - A | R | T
+    'kwi': '045/03', // Brigade `{Route}/{BrigadeNumber}`
+};
+
+const _shape = {
+    'VL': {
+        'V': [_example],
+    },
+};
+
+// TODO add safeguards
+const vehicleMapping = {
+    id: 'id',
+    sideNumber: 'nb',
+    routeNumber: 'nr',
+} as const;
+
+export type VehicleApi = typeof _shape;
+
+export type RouteInfo = {
+    number: string;
+    destination: string;
+    variant: string;
+    direction: RouteDirection;
+    id: string;
+};
+
+export type Position = {
+    longitude: number;
+    latitude: number;
+};
+
+// TODO
+export type VehicleFlags = null;
+
+// TODO
+export type VehicleStatus = null;
+
+export type Vehicle = {
+    id: string;
+    sideNumer: string;
+    position: Position;
+    _positionP: Position;
+    status: VehicleStatus;
+    current: RouteInfo;
+    delay: number;
+    next: RouteInfo; // TODO Optional
+    flags: VehicleFlags;
+    type: TransportMode;
+};
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,12 @@ import { DatabaseSync } from 'node:sqlite';
 
 import { ScheduleDatabase } from './lib/db/sql.ts';
 import { Schedule } from './lib/schedule/schedule.ts';
+import getVehicles from './lib/vehicles/vehicles.ts';
 
 if (import.meta.main) {
     console.log('Starting ZDiTM Thing...');
 
-    // console.log(await generateToken(LUBLIN_AGE));
-    // console.log(await generateHeaders(LUBLIN_AGE));
-
-    // Deno.writeTextFile("test.json", JSON.stringify((await getVehicles())));
-
     // ^ Schedule file
-    const filename = './schedule_2025_10-05.sql';
     // const filename = './schedule_2025_10-05.sql';
     // const schedule = await getSchedule(filename);
 
@@ -22,61 +17,17 @@ if (import.meta.main) {
     //     exit();
     // }
 
+    // const filename = './schedule_2025_10-05.sql';
     // const db = new DatabaseSync(filename, { readOnly: true, open: true });
 
-    const schedule = new Schedule(
-        new ScheduleDatabase(new DatabaseSync(filename, { readOnly: true, open: true }))
-    );
+    // const schedule = new Schedule(
+    //     new ScheduleDatabase(new DatabaseSync(filename, { readOnly: true, open: true }))
+    // );
+    const vehicles = await getVehicles();
 
-    // console.log(schedule);
+    await Deno.writeTextFile("vehicles.json", JSON.stringify(vehicles))
+    console.log(vehicles);
 
-    // console.log(parseStopDescription({
-    //     id: 103,
-    //     id_ul: 31,
-    //     nazwa: 'SAPIEHY 02',
-    //     numer: 1832,
-    //     slupek: null,
-    //     odjazdy: 1,
-    //     lon: 22.57234,
-    //     lat: 51.26038,
-    //     transport: 'A',
-    //     linieA: '17,24,31',
-    //     linieT: '',
-    //     linieR: '',
-    //     sort: 1031,
-    //     kierunek: 'JABŁONNA  URZĄD GMINY, OS.PORĘBA',
-    //     typ_tab: 0,
-    // }));
-    // console.log(schedule.getLines())
-
-    // console.log(metaSql);
-    // schedule.getStops();
-
-    // Deno.writeTextFile("./stops.json", JSON.stringify(schedule.generateStops()))
-    // Deno.writeTextFile("./routes.json", JSON.stringify(schedule.generateRoutes()))
-    // // Deno.writeTextFile("./points.json", JSON.stringify(schedule.generateSalesPoints()))
-
-    // !
-    schedule.saveSchedule();
-
-    // console.log(schedule.generateConfig());
-
-    // console.log(splitDeparturesString(test).map(timeString => toDepartureTime(timeString)));
-
-    // type ScheduleMetadata = {
-    //     validFrom: string;
-    //     version: number;
-    //     generation: number;
-    // };
-
-    // console.log(meta);
-    // const saveDate = Temporal.Now.plainDateISO(TIME_ZONE);
-    // const newDatabase = `schedule_${saveDate}_${meta.validFrom}_${meta.version}_${meta.generation}.sqlite`;
-    // console.log(newDatabase);
-
-    // await Deno.copyFile(filename, `./${newDatabase}`);
-
-    // console.log(compareSchedule(4563, 1));
     // Download database
     // Set version and gen
     // Save database under a name ???_{DATE}_{FROM}_{VERSION}_{GEN}.sqlite
