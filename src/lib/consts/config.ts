f6cc2965cff9be958dc48c4c661f7ff686163619
rafal_milichiewicz <53575748+rafalmilichiewicz@users.noreply.github.com>
2025-10-13 13:14:12 +0200

    Added defaultCity

--- src/lib/consts/config.ts
+++ src/lib/consts/config.ts
@@ -1,3 +1,5 @@
+import { generateAgeHeader } from '../api/token/age.ts';
+import generateCityOffset from '../api/token/city-offset.ts';
 import { CITIES } from './cities.ts';
 import { ENV_VARS } from './env_vars.ts';
 
@@ -14,6 +16,8 @@ type EnvVarValues<T> = {
     [K in keyof T]: T[K] extends Record<string, string> ? { [P in keyof T[K]]: unknown } : unknown;
 };
 
+const defaultCity = CITIES.LUBLIN;
+
 export const CONFIG = {
     API: {
         BASE_URL: Deno.env.get(ENV_VARS.API.BASE_URL) ?? CITIES.LUBLIN.url,
@@ -25,14 +29,14 @@ export const CONFIG = {
     // TODO Add parsing (No CITY.CODE => Lublin else CITY.CODE present => Compute)
     // Currently assumes (No value => Lublin)
     CITY: {
-        CODE: Deno.env.get(ENV_VARS.CITY.CODE) ?? CITIES.LUBLIN.code,
+        CODE: Deno.env.get(ENV_VARS.CITY.CODE) ?? defaultCity.code,
         OFFSET:
             Deno.env.get(ENV_VARS.CITY.OFFSET) !== undefined
                 ? parseIntEnv(Deno.env.get(ENV_VARS.CITY.OFFSET) as string)
-                : 376,
+                : generateCityOffset(defaultCity.code),
         AGE:
             Deno.env.get(ENV_VARS.CITY.AGE) !== undefined
                 ? parseIntEnv(Deno.env.get(ENV_VARS.CITY.AGE) as string)
-                : 436,
+                : generateAgeHeader(generateCityOffset(defaultCity.code)),
     },
 } as const satisfies EnvVarValues<typeof ENV_VARS>;
